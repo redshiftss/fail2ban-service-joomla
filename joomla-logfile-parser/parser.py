f6b3f126d9a8ae8f6b4d7152d7eb3d2b5f83b9c5
laura pircalaboiu <laura.pircalaboiu@yahoo.it>
2022-04-01 11:32:45 +0200

    change file parsing

--- joomla-logfile-parser/parser.py
+++ joomla-logfile-parser/parser.py
@@ -4,13 +4,13 @@ import requests
 from collections import Counter
 
 
-path_to_logfile = "joomla-logfile-parser/logfile.csv"
+path_to_logfile = "joomla-logfile-parser/not_user_log.log"
 timeframe = int(sys.argv[1])
 allowed_tries = int(sys.argv[2])
 
 def check_logfile():
     f = open(path_to_logfile, "r")
-    lines = f.read().split("\n")[1:]
+    lines = f.read().split("\n")[6:]
     acts = list(map(lambda x: to_action(x), lines))
     login_attempts_within_x = list(filter(lambda action: action.is_login_attempt and is_within(datetime.utcnow(), action.Date, timeframe), acts))
     ip_counter = Counter(a.IP for a in login_attempts_within_x)
@@ -22,24 +22,25 @@ def check_logfile():
             requests.post('http://localhost:8080/api/block/{}?key=4538f19b-9575-4cca-beba-a4382a37707d'.format(i))
 
 def to_action(str):
-    str_bits = str.split(",")
-    return Action(str_bits[0], str_bits[1], str_bits[2], str_bits[3][1:-1], str_bits[4], str_bits[5])
+    str_bits = str.split("\t")
+    print(str_bits)
+    return Action(str_bits[0], str_bits[1][5:], str_bits[2])
 
 def is_within(dt1, dt2, tf):
     delta = dt1 - dt2
     return delta.total_seconds() / 60 < tf
 
 class Action:
-    def __init__(self, Id, Act, Extension, Date, Name, IP):
-        self.Id = Id
+    def __init__(self, Date, IP, Act):
+        # self.Id = Id
         self.Act = Act
-        self.Extension = Extension
-        self.Date = datetime.strptime(Date, "%Y-%m-%d %H:%M:%S UTC")
+        # self.Extension = Extension
+        self.Date = datetime.strptime(Date, "%Y-%m-%dT%H:%M:%S+00:00")
         # self.Date = Date
-        self.Name = Name
+        # self.Name = Name
         self.IP = IP
     
     def is_login_attempt(self):
-        return self.Act == "A failed attempt was made to login as admin to site";
+        return self.Act == "Username and password do not match or you do not have an account yet.";
 
 check_logfile()
